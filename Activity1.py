cab1cd8e0de52d7c1946b48f08628de471546285
mridul3601-cell <mridul3601@gmail.com>
2026-01-10 17:55:04 -0500

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,9 @@
+tree1 = 67
+tree2 = 69
+tree3 = 61
+tree4 = 41
+tree5 = 89
+sum = tree1 + tree2 + tree3 + tree4 + tree5
+print ("the sum is",sum)
+average = sum / 5
+print ("the average is", average)
