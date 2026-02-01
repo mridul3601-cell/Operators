d500d5adeb194c75ad72889fbe556114b02db384
mridul3601-cell <mridul3601@gmail.com>
2026-02-01 17:22:09 -0500

    Add files via upload

---
+++ Activity8.py
@@ -0,0 +1,7 @@
+number= int(input("enter in a number to check"))
+print ("number to be checked:",number)
+
+if number %2==0:
+ print ("this is a even number")
+else :
+ print ("this is a odd number")
