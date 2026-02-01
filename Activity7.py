5ab786a2cf2b8007a1aaf61c48e65e9e5564988d
mridul3601-cell <mridul3601@gmail.com>
2026-02-01 17:15:14 -0500

    Add files via upload

---
+++ Activity7.py
@@ -0,0 +1,8 @@
+i = int(input('enter any number'))
+if i > 15 :
+    print("number is greater than 15")
+    print ("I'm a if block")
+else :
+    print("number is not greater than 15")
+    print ("im a else block")
+print("I'm outside of if and else block")
