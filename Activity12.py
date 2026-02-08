9aad7d849a7f946c6b3daf604da5a96663780906
mridul3601-cell <mridul3601@gmail.com>
2026-02-08 17:14:59 -0500

    Add files via upload

---
+++ Activity12.py
@@ -0,0 +1,23 @@
+A=10
+B=12
+C=12
+
+print(A!=B)
+print(B!=A)
+
+
+A="python"
+B="Coding"
+
+if A != B :
+    print ("A and B are different")
+
+
+A=4
+B=5
+
+if (A==1) != (B==5) :
+    print("hello")
+A= int(input("enter a number"))
+if A%2 != 0:
+    print("A is not a even number")
