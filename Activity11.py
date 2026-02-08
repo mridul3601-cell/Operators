0f29843b7eafa2aa536730219291a686894d2778
mridul3601-cell <mridul3601@gmail.com>
2026-02-08 17:03:38 -0500

    Add files via upload

---
+++ Activity11.py
@@ -0,0 +1,23 @@
+a=11
+b=-11
+c=0
+
+if a and b and c:
+    print("all the boolean values as true")
+else:
+    print("atlease one boolean value as false")
+
+
+
+a=11
+b=-11
+c=0
+
+if a > 0 or b > 0:
+    print("either number is greater than 0")
+else:
+    print('no number is greater than zero')    
+if b > 0 or c > 0:
+      print("either number is greater than 0")
+else:
+    print('no number is greater than zero')   
