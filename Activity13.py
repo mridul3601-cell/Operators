8514a775cbd6ea6cc8157d646e630564487b2a15
mridul3601-cell <mridul3601@gmail.com>
2026-02-08 17:29:05 -0500

    Add files via upload

---
+++ Activity13.py
@@ -0,0 +1,19 @@
+weight=float(input("enter your weight in kg's:"))
+height=float(input("enter your height in cm:"))
+
+BMI= weight / (height/100)**2
+
+print("your Bmi is",BMI)
+if BMI<=18.4:
+    print("you are underweight")
+elif BMI<= 24.9:
+    print("you are healthy")
+elif BMI<=29.9:
+    print("you are overweight")
+elif BMI<=34.9:
+    print("you are severly overweight")
+elif BMI<=39.9:
+    print("you are obese")
+else:
+    print("you are severly obese")
+
