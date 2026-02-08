abb0ac23bc7445f2b28cde7eda598f18df16e75a
mridul3601-cell <mridul3601@gmail.com>
2026-02-08 16:11:43 -0500

    Add files via upload

---
+++ Activity10.py
@@ -0,0 +1,7 @@
+temperature = int(input("Enter the temperature: "))
+
+if temperature >= 22:
+    print("You can wear light and soft clothes.")
+else:
+    print("It is cold. Wear a jacket or pullover.")
+    
