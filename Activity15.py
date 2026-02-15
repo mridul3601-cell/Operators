9324a625c28178fbb89eaaceeb6ab1933f2cfd24
mridul3601-cell <mridul3601@gmail.com>
2026-02-15 17:06:04 -0500

    Add files via upload

---
+++ Activity15.py
@@ -0,0 +1,36 @@
+x=5
+
+if (type(x) is int):
+
+    print("True")
+
+else:
+
+    print('false')
+
+
+x=5.5
+
+if(type(x) is not float):
+
+    print("true")
+
+else:
+
+    print("false")
+
+x=20
+
+y=20
+
+if (x is y):
+
+    print("x and y are the same")
+
+
+
+y=30
+
+if (x is not y):
+
+    print (" x and y are different")
