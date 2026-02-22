52debef0e9bce155e8a02227b61bb6ae0faa742e
mridul3601-cell <mridul3601@gmail.com>
2026-02-22 17:40:24 -0500

    Add files via upload

---
+++ Activity21.py
@@ -0,0 +1,13 @@
+mean1=38
+Wrongnumber = 36
+correctnumber= 56
+totalnumber= 40
+
+sum=mean1*totalnumber
+print("sum of 40 number",sum)
+
+num2=sum-((Wrongnumber))-((correctnumber)) 
+print("sum-((Wrongnumber))-((correctnumber)):",)
+
+mean2=num2/totalnumber
+print(mean2)
