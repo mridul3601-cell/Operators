bd7558aef60d17c09bfede69dbd1f758e1f3f7cb
mridul3601-cell <mridul3601@gmail.com>
2026-02-22 17:14:17 -0500

    Add files via upload

---
+++ Activity19.py
@@ -0,0 +1,25 @@
+v=4 
+
+b=9
+
+c=3
+
+a=7
+
+j=6
+
+j=(v+b)* c/a 
+
+print("value of (v+b)* c/a", j)
+
+
+
+
+name = "alex"
+age=0
+
+if name == "alex" or name == "John" and age >=2:
+    print("Welcome")
+else :
+    print("bye")
+    
