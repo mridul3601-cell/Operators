3c17a155e2600a61b19505c659230f39c095f68f
mridul3601-cell <mridul3601@gmail.com>
2026-03-15 16:07:09 -0400

    Add files via upload

---
+++ Activity26.py
@@ -0,0 +1,6 @@
+age = int(input("Enter student's age: "))
+
+if age >= 10 and age <= 20:
+    print("Student is allowed to enroll in the class.")
+else:
+    print("Student cannot enroll in the class.")
