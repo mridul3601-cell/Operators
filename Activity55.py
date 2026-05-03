5f7792b829b55f1e70d0ffb7c6dc1b410c3d8e2e
mridul3601-cell <mridul3601@gmail.com>
2026-05-03 18:27:34 -0400

    Add files via upload

---
+++ Activity55.py
@@ -0,0 +1,7 @@
+def P_R():
+    print("please enter a length")
+    print("please enter a width")
+L = int(input("please enter a length "))
+W = int(input("please enter a width "))
+P = (L + W) * 2
+print(P)
