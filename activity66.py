fa4442589d589eb18fe140fc9e57be44fabb7355
mridul3601-cell <mridul3601@gmail.com>
2026-07-05 17:04:23 -0400

    Add files via upload

---
+++ activity66.py
@@ -0,0 +1,13 @@
+def change(bill, paid):
+    if paid < bill:
+        return "Not enough money"
+
+    pass
+
+    for i in range(1):
+        continue
+        break
+
+    return paid - bill
+
+print(change(2.50, 4.00))
