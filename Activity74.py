08feecfcec2a1aba402942fcb515dc17af2a8398
mridul3601-cell <mridul3601@gmail.com>
2026-07-12 18:31:44 -0400

    Add files via upload

---
+++ Activity74.py
@@ -0,0 +1,8 @@
+import math
+print("the floor and ceiling value of 23.56 are:")
+str = (math.ceil(23.56))+','+str(math.floor(23.56))
+x=10
+y=-15
+print("the value of x after copying the sign from y is:")
+str(math.copysign(x,y))
+print("the absolute value of -96 and 56 are"+
